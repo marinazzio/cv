a17a420e8b406a7469f846594f64394ab742f754
Denis Kiselev <denis.kiselyov@gmail.com>
2024-08-01 18:18:58 +0700

    Simplify locale logic

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// const isProd = process.env.NODE_ENV === 'production';
-// const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-
-// export default {
-//     basePath: isProd ? `/${repoName}` : '',
-//     assetPrefix: isProd ? `/${repoName}/` : '',
-//     trailingSlash: true
-// };
